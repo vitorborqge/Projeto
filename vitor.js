ffbccaaa2376eb0b01b78832609a6f128956d41e
vitor borges vieira <101013550+vitorborqge@users.noreply.github.com>
2022-04-09 11:48:29 -0300

    Update vitor.js

--- vitor.js
+++ vitor.js
@@ -1,32 +1,37 @@
 class Pessoas {
+
+    salvar(){
+        alert('Cadastrado com Sucesso');
+        /// ...
+      }
        FuncaoPrincipal() { // função pra criar o json
-        let Pessoas = {}
+        let pessoas = {}
         
-        Pessoas.id = this.id;
-        Pessoas.CPF = document.getElementById('CPF').value;
-        Pessoas.nome = document.getElementById('nome').value;
-        Pessoas.Sobrenome = document.getElementById('Sobrenome').value;
-        Pessoas.DataDeNascimento = document.getElementById('data').value;
-        Pessoas.Idade = document.getElementById('idade').value;
-        Pessoas.ÉMaiorDeIdade = document.getElementById('exampleRadios1').value;
-        Pessoas.EMaiorDeIdade = document.getElementById('exampleRadios2').value;
-        Pessoas.Vaga = document.getElementById('vaga').value;
+        pessoas.id = this.id;
+        pessoas.CPF = document.getElementById('CPF').value;
+        pessoas.nome = document.getElementById('nome').value;
+        pessoas.Sobrenome = document.getElementById('Sobrenome').value;
+        pessoas.DataDeNascimento = document.getElementById('data').value;
+        pessoas.Idade = document.getElementById('idade').value;
+        pessoas.ÉMaiorDeIdade = document.getElementById('exampleRadios1').value;
+        pessoas.EMaiorDeIdade = document.getElementById('exampleRadios2').value;
+        pessoas.Vaga = document.getElementById('vaga').value;
     
-        return Pessoas;
+        return pessoas;
         }
         constructor(){
             this.id = 1;
             this.arrayPessoas = [];
             }
     
-    CliqueAqui(Pessoas){
+    CliqueAqui(pessoas){
         let Pessoa = this.function();
         
-        if(this.validaCampos(Pessoas) == true) { // função que da uma olhada se os campos estão corretos ai da um alerta
+        if(this.validaCampos(pessoas) == true) { // função que da uma olhada se os campos estão corretos ai da um alerta
             alert('salvar')
         }
-        if(this.validaCampos(Pessoas)) {
-            this.adicionar(Pessoas);
+        if(this.validaCampos(pessoas)) {
+            this.adicionar(pessoas);
         }
         console.log(this.arrayPessoas);
     
@@ -34,38 +39,38 @@ class Pessoas {
         console.log(Pessoas);
         }
         
-        adicionar(Pessoas) { //função que pega o array de pessoas
+        adicionar(pessoas) { //função que pega o array de pessoas
             this.arrayPessoas.push(Pessoas);
             this.id++;
         }
     
     
-        validaCampos(Pessoas){   // função que valida os campos
+        validaCampos(pessoas){   // função que valida os campos
         
         let msg = '';
         
-           if(Pessoas.nome == '') {
+        if(pessoas.nome == '') {
                   msg += '- Informe seu nome \n';
         }
-        if(Pessoas.CPF == '') {
+        if(pessoas.CPF == '') {
                   msg += '- Informe seu CPF \n';
         }
-        if(Pessoas.Sobrenome == '') {
+        if(pessoas.Sobrenome == '') {
                   msg += '- Informe seu Sobrenome \n';
         }
-        if(Pessoas.DataDeNascimento == '') {
+        if(pessoas.DataDeNascimento == '') {
                   msg += '- Informe sua Data de Nascimento \n';
         }
-        if(Pessoas.Idade == '') {
+        if(pessoas.Idade == '') {
                   msg += '- Informe sua Idade \n';
         }
-        if(Pessoas.ÉMaiorDeIdade == '') {
+        if(pessoas.ÉMaiorDeIdade == '') {
                   msg += '- Informe se você é maior de Idade ou não \n';
         }
-        if(Pessoas.EMaiorDeIdade == '') {
+        if(pessoas.EMaiorDeIdade == '') {
                   msg += '- Informe se você é maior de Idade ou não \n';
         }
-        if(processo.Vaga == '') {
+        if(pessoas.Vaga == '') {
                   msg += '- Informe sua Vaga desejada \n';
     }
     
