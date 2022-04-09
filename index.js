bd94784a1eda2a5eeb22e5ed20fac469b8b8c78d
vitor borges vieira <101013550+vitorborqge@users.noreply.github.com>
2022-04-09 10:40:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function salvar(){
+    alert('Cadastrado com Sucesso');
+    /// ...
+  }
+  
+         
+  if( inputs >= 15 ) {
+          
+    alert("O nº de candidatos já atingiu o limite permitido");
+  }
+  else {
+    alert("Cadastrado com Sucesso!");
+  }
+  
