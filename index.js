9ed62c9b1d5238c7184bf66b6c6ae03ff78277aa
vitor borges vieira <101013550+vitorborqge@users.noreply.github.com>
2022-04-09 11:48:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
 
-function salvar(){
-    alert('Cadastrado com Sucesso');
-    /// ...
-  }
-  
-         
-  if( inputs >= 15 ) {
-          
-    alert("O nº de candidatos já atingiu o limite permitido");
-  }
-  else {
-    alert("Cadastrado com Sucesso!");
-  }
-  
